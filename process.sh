c371e09fc7bc242ae9547374b9980e000a52ff55
Hugo <9675966+NotaInutilis@users.noreply.github.com>
2023-08-08 01:11:36 +0200

    Actually fix the intro thing
    "ask chatgpt for help" they told me
    "oh it can actually explain what the command is doing" i thougth
    "chatgpt is an utter and complete fool" i then realized, cursing my so-called friends

--- process.sh
+++ process.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sed 's/^www.//g;s/^.*$/*:\/\/*.\0\/*/g' domains.txt > domains_uBlacklist.txt
 sort -o domains_uBlacklist.txt{,}
-sed -i 'ir list_info.txt' domains_uBlacklist.txt
+cat list_info.txt domains_uBlacklist.txt >out && mv out domains_uBlacklist.txt
