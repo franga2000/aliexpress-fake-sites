b2f1e360fc9cac3deefe1ce22e3efa166225aa27
Hugo <9675966+NotaInutilis@users.noreply.github.com>
2023-08-08 02:31:44 +0200

    Moved the www cleanup task from the block list to the domains file

--- process.sh
+++ process.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-sed 's/^www.//g;s/^.*$/*:\/\/*.\0\/*/g' domains.txt > domains_uBlacklist.txt
+sed -i 's/^www.//g' domains.txt
+sed 's/^.*$/*:\/\/*.\0\/*/g' domains.txt > domains_uBlacklist.txt
 sort -o domains_uBlacklist.txt{,}
 cat list_info.txt domains_uBlacklist.txt >out && mv out domains_uBlacklist.txt
