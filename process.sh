91e84c57428a9cbee6bdec145e81e22f2d87772d
Hugo <9675966+NotaInutilis@users.noreply.github.com>
2023-08-08 17:04:13 +0200

    Removed sorting and regenerated blocklist

--- process.sh
+++ process.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 sed -i 's/^www.//g' domains.txt
 sed 's/^.*$/*:\/\/*.\0\/*/g' domains.txt > domains_uBlacklist.txt
-sort -o domains_uBlacklist.txt{,}
 cat list_info.txt domains_uBlacklist.txt >out && mv out domains_uBlacklist.txt
