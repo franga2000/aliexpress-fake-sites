b4f8214ce6e182e730effbf41e3c1f5bf778442c
Nota Inutilis <9675966+NotaInutilis@users.noreply.github.com>
2023-08-25 16:57:09 +0200

    Update process.sh

--- process.sh
+++ process.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-sed -i 's/^www.//g' domains.txt
-sed 's/^.*$/*:\/\/*.\0\/*/g' domains.txt > domains_uBlacklist.txt
-cat list_info.txt domains_uBlacklist.txt >out && mv out domains_uBlacklist.txt
+
+# Cleanup `domains.txt`.
+sed -i 's/^www\.//g' domains.txt
+cat -n domains.txt | sort -uk2 | sort -nk1 | cut -f2- > domains_temp.txt
+mv domains_temp.txt domains.txt
+
+# Generate uBlacklist filter.
+sed 's/^.*$/*:\/\/*.\0\/*/g' domains.txt > domains_uBlacklist_temp.txt
+cp list_info.txt domains_uBlacklist.txt
+cat domains_uBlacklist_temp.txt >> domains_uBlacklist.txt
+rm domains_uBlacklist_temp.txt
