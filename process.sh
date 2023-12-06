a7ef6b353cb49b39a5f68248cae34d4e8384adf6
Hugo <hugo@notainutilis.fr>
2023-12-06 23:05:23 +0100

    Update process.sh

--- process.sh
+++ process.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Normalizes URLs in `domains.txt` into domains: lowercases, remove leading spaces, protocol (`x://`) `www.` subdomains, everything after `/`.
+# Normalizes URLs in `domains.txt` into domains: lowercases, remove leading spaces, protocol (`x://`), `www.` subdomains, everything after `/`.
 sed -i 's/.*/\L&/; s/^[[:space:]]*//i; s/^.*:\/\///i; s/^[.*]*//i; s/^www\.//i; s/\/.*$//i' domains.txt
 cat -n domains.txt | sort -uk2 | sort -nk1 | cut -f2- > domains_temp.txt
 mv domains_temp.txt domains.txt
